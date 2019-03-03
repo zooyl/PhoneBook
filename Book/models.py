261abd1151ac88fbbfc6dfeb59aa773e0d7631ca
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-03-03 13:47:44 +0100

    Alpha 1.0

---
+++ Book/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+c_type = (
+    (1, 'Mobile'),
+    (2, 'Work'),
+    (3, 'Home'),
+    (4, 'Fax'),
+    (5, 'Personal')
+)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128, blank=True)
+    description = models.TextField(blank=True)
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=128, blank=True)
+    street = models.CharField(max_length=128, blank=True)
+    house_nr = models.IntegerField(blank=True)
+    flat_nr = models.IntegerField(blank=True)
+    occupant_key = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='occupant_key')
+
+
+class Phone(models.Model):
+    number = models.IntegerField(unique=True)
+    type = models.CharField(max_length=64, choices=c_type, default=1)
+    phone_key = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='phone_key')
+
+
+class Email(models.Model):
+    email = models.EmailField(max_length=64, unique=True, blank=True)
+    email_type = models.CharField(max_length=64, choices=c_type, default=5)
+    email_key = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='email_key')
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=64, blank=True)
+    group_key = models.ManyToManyField(Person)
---
+++ Book/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from Book.models import Person, Phone, Email, Address, Group
+from django.db import IntegrityError, DataError
+
+
+def home(request):
+    person = Person.objects.all().order_by('name')
+    return render(request, "display_person.html", {'person': person})
+
+
+class NewBasic(View):
+
+    def get(self, request):
+        return render(request, 'add_basic.html')
+
+    def post(self, request):
+        Person.objects.create(name=request.POST['name'], surname=request.POST['surname'],
+                              description=request.POST['description'])
+        last = Person.objects.latest('id')
+        return redirect(f'/details/basic/{last.id}')
+
+
+class NewAdvanced(View):
+
+    def get(self, request):
+        last = Person.objects.latest('id')
+        return render(request, 'edit_person.html', {'last': last})
+
+    def post(self, request):
+        try:
+            id = request.POST['id']
+            name = request.POST['name']
+            surname = request.POST['surname']
+            description = request.POST['description']
+            # if request is None:
+            # Edit for basic informations
+            edit = Person.objects.get(id=id)
+            edit.name = name
+            edit.surname = surname
+            edit.description = description
+            edit.save()
+            Address.objects.create(city=request.POST['city'], street=request.POST['street'],
+                                   house_nr=request.POST['house_nr'], flat_nr=request.POST['flat_nr'],
+                                   occupant_key=edit)
+            Email.objects.create(email=request.POST['email'], email_key=edit)
+            Phone.objects.create(number=request.POST['phone_number'], phone_key=edit)
+            Group.objects.create(name=request.POST['group'])
+            added = "Person added"
+            return render(request, "back_button.html", {'added': added})
+        except IntegrityError:
+            unique = "E-mail already taken, it must be unique"
+            return render(request, "edit_person.html", {'unique': unique})
+        except DataError:
+            number = "Number out of range"
+            return render(request, "edit_person.html", {'number': number})
+
+
+def basic_details(request, id):
+    person = Person.objects.get(id=id)
+    return render(request, "basic_details.html", {'person': person})
+
+def full_details(request, id):
+    person = Person.objects.get(id=id)
+    address = Address.objects.get(id=id)
+    email = Email.objects.get(id=id)
+    phone = Phone.objects.get(id=id)
+    group = Group.objects.get(id=id)
+    if request.method == "POST":
+        if request.POST == ['delete']:
+            person.delete()
+            redirect("/")
+        elif request.POST == ['edit']:
+            render(request, 'edit_person.html')
+    else:
+        return render(request, "details.html", {'id': id, 'person': person, 'address': address,
+                                                'email': email, 'phone': phone, 'group': group})
